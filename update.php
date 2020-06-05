9c41cfdeaf14d2af428cddad392eb744689558ab
Paul Staroch <paulchen@rueckgr.at>
2020-06-06 00:39:18 +0200

    Improve support for IPv6: allow setting only IPv4 or IPv6 per request

--- update.php
+++ update.php
@@ -1,4 +1,5 @@
 <?php
+# TODO introduce logging
 function db_query($query, $parameters = array()) {
 	global $db;
 
@@ -22,6 +23,8 @@ function db_query($query, $parameters = array()) {
 }
 
 function db_error($error, $stacktrace, $query, $parameters) {
+#	print_r($error);
+#	print_r($stacktrace);
 	// TODO
 	/*
 	global $config;
@@ -42,7 +45,7 @@ function db_error($error, $stacktrace, $query, $parameters) {
 
 	mail($report_email, $subject, $message, $headers);
 	*/
-	header('HTTP/1.1 500 Internal Server Error');
+#	header('HTTP/1.1 500 Internal Server Error');
 	echo "A database error has just occurred. Please don't freak out, the administrator has already been notified.";
 	die();
 }
@@ -51,6 +54,10 @@ require_once(dirname(__FILE__) . '/config.php');
 $db = new PDO("mysql:dbname=$db_name;host=$db_host;port=$db_port", $db_username, $db_password);
 db_query('SET NAMES utf8');
 
+$custom = false;
+$ip = '';
+$ip6 = '';
+# TODO get rid of this?
 if(isset($_REQUEST['TZOName'])) {
 	foreach(array('TZOName', 'Email', 'TZOKey', 'IPAddress') as $key) {
 		if(!isset($_REQUEST[$key])) {
@@ -65,22 +72,40 @@ if(isset($_REQUEST['TZOName'])) {
 	$ip = $_REQUEST['IPAddress'];
 	$ip6 = isset($_REQUEST['IP6Address']) ? $_REQUEST['IP6Address'] : '';
 }
-else if(isset($_REQUEST['system']) && $_REQUEST['system'] == 'custom') {
+else if(isset($_REQUEST['system']) && $_REQUEST['system'] == 'custom') { # TODO get rid of 'system' parameter?
 	$host = $_REQUEST['hostname'];
-	$ip = $_REQUEST['myip'];
-	$ip6 = isset($_REQUEST['myip6']) ? $_REQUEST['myip6'] : '';
+	if(isset($_REQUEST['myip'])) {
+		$ip = $_REQUEST['myip'];
+		$ip6 = isset($_REQUEST['myip6']) ? $_REQUEST['myip6'] : '';
+	}
+	else {
+		$custom = true;
+		# https://stackoverflow.com/questions/1448871/how-to-know-which-version-of-the-internet-protocol-ip-a-client-is-using-when-c
+		if(filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+			$ip6 = $_SERVER['REMOTE_ADDR'];
+		}
+		else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+	}
+	# TODO make HTTP basic auth work again
 	$username = $_REQUEST['username'];
 	$password = $_REQUEST['password'];
 #	$username = $_SERVER['PHP_AUTH_USER'];
 #	$password = $_SERVER['PHP_AUTH_PW'];
-	echo "$host $ip $username $password";
+	echo "$host $ip $ip6 $username $password";
 }
 else {
+	# TODO
 #	header('HTTP/1.0 400 Bad Request');
 	die('Bad Request 1');
 }
 
+if(!$ip && !$ip6) {
+	// TODO wtf
+}
 if(!preg_match('/[a-z]+/', $host)) {
+	# TODO wtf
 #	header('HTTP/1.0 400 Bad Request');
 	die('Bad Request 2');
 }
@@ -93,17 +118,18 @@ if(!preg_match('/[a-zA-Z0-9]+/', $password)) {
 	die('Bad Request 4');
 }
 $source_ip = $_SERVER['REMOTE_ADDR'];
-if($ip == '') {
+if(!$custom && $ip) {
 	$ip = $source_ip;
 }
 
-if(!preg_match('/[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]/', $ip)) {
+if($ip && !preg_match('/[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]/', $ip)) {
 	header('HTTP/1.0 400 Bad Request');
 	die('Bad Request');
 }
+# TODO validate $ip6
 
 $data = db_query('SELECT id, password FROM accounts WHERE username = ? AND active = 1', array($username));
-if(count($data) != 1 || crypt($password, $data[0]['password']) != $data[0]['password']) {
+if(count($data) != 1 || !password_verify($password, $data[0]['password'])) {
 	header('HTTP/1.0 403 Forbidden');
 	die('Forbidden');
 }
@@ -117,12 +143,27 @@ if(count($data) != 1) {
 $host_id = $data[0]['id'];
 
 db_query('INSERT INTO updates (host, user, source_ip, new_ip, new_ip6) VALUES (?, ?, ?, ?, ?)', array($host_id, $user_id, $source_ip, $ip, $ip6));
-db_query('INSERT INTO current (host, ip, ip6) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE ip = ?, ip6 = ?', array($host_id, $ip, $ip6, $ip, $ip6));
+if($ip && $ip6) {
+	db_query("INSERT INTO current (host, ip, ip6) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE ip = ?, ip6 = ?", array($host_id, $ip, $ip6, $ip, $ip6));
+}
+else if($ip) {
+	db_query("INSERT INTO current (host, ip, ip6) VALUES (?, ?, '') ON DUPLICATE KEY UPDATE ip = ?", array($host_id, $ip, $ip));
+}
+else {
+	db_query("INSERT INTO current (host, ip, ip6) VALUES (?, '', ?) ON DUPLICATE KEY UPDATE ip6 = ?", array($host_id, $ip6, $ip6));
+}
 
-$call = "#!/bin/bash\necho -e \"update delete $host.ddns.rueckgr.at A\\nupdate delete $host.ddns.rueckgr.at AAAA\\nupdate add $host.ddns.rueckgr.at 60 A $ip\\n";
-#if($ip6) {
-#	$call .= "update add $host.ddns.rueckgr.at 60 AAAA $ip6\\n";
-#}
+$call = "#!/bin/bash\n";
+$call .= "echo -e \"";
+if($ip) {
+	$call .= "update delete $host.ddns.rueckgr.at A\\n";
+	$call .= "update add $host.ddns.rueckgr.at 60 A $ip\\n";
+}
+# TODO remove this exception
+if($ip6 && $username != 'Thonie6o') {
+	$call .= "update delete $host.ddns.rueckgr.at AAAA\\n";
+	$call .= "update add $host.ddns.rueckgr.at 60 AAAA $ip6\\n";
+}
 $call .= "send\"|nsupdate\n";
 $filename = tempnam('/tmp','nsupdate_');
 file_put_contents($filename, $call);
@@ -130,5 +171,6 @@ chmod($filename, 0700);
 exec($filename);
 unlink($filename);
 header('HTTP/1.0 200 OK');
+# TODO improve this
 die("TZOName=$host IPAddress=$ip Expiration=12/31/2020");
 
